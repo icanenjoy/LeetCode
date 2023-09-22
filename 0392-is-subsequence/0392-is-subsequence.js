cd48914531090f20bbb0bf61118b4f4b9ad14937
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-09-22 12:42:55 +0900

    Time: 62 ms (12.84%), Space: 41.8 MB (64.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -5,16 +5,13 @@
  */
 var isSubsequence = function(s, t) {
     s = s.split("");
-    t = t.split("");
+    t = t.split(""); 
     while(1){
         
-        if(s[0]===t[0]) s.shift();
-        t.shift();
-        if(s.length===0 || t.length===0) break;
+        if(s[0]===t[0]) s.shift(); //같은 글자면 s하나 없애기
+        t.shift(); //t는 계속 줄이기
+        if(s.length===0 || t.length===0) break; //두 배열 중 하나라도 비워지면 종료
     }
-
-    console.log(s);
-    console.log(t);
-    return s.length? false:true;
+    return s.length? false:true; // s가 남아있지 않으면 true 있으면 false
     
 };
