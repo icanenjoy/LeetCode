15ecdeb9e9c3dc1441af290c3572f738027f21fd
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-10-04 19:00:12 +0900

    Time: 66 ms (20.97%), Space: 42.4 MB (82.80%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.js
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.js
@@ -9,14 +9,15 @@ var maxIncreaseKeepingSkyline = function(grid) {
             let Y=0;
             let X=0;
             for(let y=0;y<grid.length;y++){
-                if(Y<grid[i][y]) Y = grid[i][y];
+                if(Y<grid[i][y]) Y = grid[i][y]; //가로에서 큰값
             }
             for(let x=0;x<grid.length;x++){
-                if(X<grid[x][j]) X = grid[x][j];
+                if(X<grid[x][j]) X = grid[x][j]; // 세로에서 큰값
             }
-            ans -= grid[i][j];
-            grid[i][j] = X>Y ? Y:X;
-            ans += grid[i][j];
+            ans -= grid[i][j];  //원래 값 빼기
+            grid[i][j] = X>Y ? Y:X; // 가로 세로에서 큰값 중 작은 값을 넣음
+            ans += grid[i][j]; //증가값 더하기 
+            //결과적으로 (증가된값-원래값) = 증가된양만큼 더해줌 
         }
     }
     return ans;
