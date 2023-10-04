acdafb3e3449817ea97bbcc63ae2f160d38c0b20
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-10-04 18:27:07 +0900

    Time: 87 ms (76.92%), Space: 44.6 MB (76.07%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.js
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.js
@@ -13,7 +13,7 @@ var countPoints = function(points, queries) {
         let check = 0;
         for(let j=0;j<points.length;j++){
             //if(points[j][0]>=minX && points[j][0]<=maxX && points[j][1]>=minY && points[j][1]<=maxY){  필요 없어짐
-                if(Math.sqrt((points[j][0]-queries[i][0])**2+(points[j][1]-queries[i][1])**2) <= queries[i][2])
+                if(Math.sqrt((points[j][0]-queries[i][0])**2+(points[j][1]-queries[i][1])**2) <= queries[i][2])// 그냥 반지름 확인하면 되네..
                     check++;
             //}
         }
