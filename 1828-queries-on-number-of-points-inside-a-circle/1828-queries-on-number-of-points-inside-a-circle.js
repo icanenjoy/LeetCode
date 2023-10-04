73c8f9570308ee7e8c3df1ab29c29dbb7ce9f085
육은진 <50579709+icanenjoy@users.noreply.github.com>
2023-10-04 18:26:22 +0900

    Time: 93 ms (61.54%), Space: 45 MB (64.10%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.js
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.js
@@ -12,10 +12,10 @@ var countPoints = function(points, queries) {
         const maxY = queries[i][1]+queries[i][2];
         let check = 0;
         for(let j=0;j<points.length;j++){
-            if(points[j][0]>=minX && points[j][0]<=maxX && points[j][1]>=minY && points[j][1]<=maxY){
+            //if(points[j][0]>=minX && points[j][0]<=maxX && points[j][1]>=minY && points[j][1]<=maxY){ 
                 if(Math.sqrt((points[j][0]-queries[i][0])**2+(points[j][1]-queries[i][1])**2) <= queries[i][2])
                     check++;
-            }
+            //}
         }
         ans.push(check);
         
